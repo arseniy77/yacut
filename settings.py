3240bf1facb1035942f623deb2e01827945c05aa
Arseniy Gaintsev <arseniy.gaintsev@gmail.com>
2022-07-07 19:59:39 +0300

    tests ok_2

--- settings.py
+++ settings.py
@@ -5,8 +5,3 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
-
-
-
-# Регулярное выражение для генератора коротких ссылок
-
